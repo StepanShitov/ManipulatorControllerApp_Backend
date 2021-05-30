8fdec63f2c4ea9be4c58ffe4524340fb35155e50
Stevie <pkka.armor@gmail.com>
2021-05-30 23:32:57 +0700

    working on pages design

--- manipulator/urls.py
+++ manipulator/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    path('<username>/main_menu', views.main_menu, name='main_menu')
+    path('<username>/main_menu/', views.main_menu, name='main_menu')
 ]
