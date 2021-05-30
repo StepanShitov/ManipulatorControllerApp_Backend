832c0b620136a25ba583a4719c2203ca7f20b021
Stevie <pkka.armor@gmail.com>
2021-05-30 21:31:35 +0700

    started to work with pages and created basic views

--- manipulator/urls.py
+++ manipulator/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/login/', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('<username>/main_menu', views.main_menu, name='main_menu')
 ]
--- manipulator/views.py
+++ manipulator/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from manipulator.models import User
+from django.shortcuts import render, resolve_url
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at dead page.")
+
+
+def login(request):
+    return render(request, 'manipulator/authorisation.html')
+
+
+def main_menu(request, username):
+    return render(request, 'manipulator/main_menu.html', {'username' : username})
